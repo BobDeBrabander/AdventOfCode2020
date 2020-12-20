dd367099b529cb29c55e5e0858985d1e8a3c4d2f
BobDeBrabander <b.d.brabander@student.tue.nl>
2020-12-20 13:29:19 +0100

    day 2 + some filename refactoring

---
+++ src/day2/AOC2-part1.kt
@@ -0,0 +1,17 @@
+package day2
+
+import java.io.File
+
+fun main(){
+    val bufferedReader = File("resources/day2/aoc-input.txt").bufferedReader()
+    val lines = bufferedReader.readLines()
+    val validLines = lines.filter{
+        val parts = it.split(" ")
+        val counts = parts[0].split("-").map{it.toInt()}
+        val letter = parts[1][0]
+        val password = parts[2]
+        val letterCount = password.count { it == letter }
+        letterCount in counts[0]..counts[1]
+    }
+    println(validLines.size)
+}
